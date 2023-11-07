670b50057dd559297a243596f2e321b12aa0586b
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-11-07 14:28:58 +0100

    Test: Update Test cases

--- tests/yams.test.js
+++ tests/yams.test.js
@@ -3,13 +3,13 @@ import yamsGame from '../sources/main.js';
 
 describe('Yams Game', () => {
   it('the dice suite is a Carré', () => {
-    expect(yamsGame([4, 4, 4, 4, 2])).toEqual(35);
+    expect(yamsGame([4, 4, 2, 4, 4])).toEqual(35);
   });
   it('the dice suite is a Brelan', () => {
-    expect(yamsGame([2, 2, 2, 5, 6])).toEqual(28);
+    expect(yamsGame([4, 2, 4, 5, 4])).toEqual(28);
   });
   it('the dice suite is a Full', () => {
-    expect(yamsGame([2, 2, 3, 3, 3])).toEqual(30);
+    expect(yamsGame([3, 2, 3, 2, 3])).toEqual(30);
   });
   it('the dice suite is a Grande Suite', () => {
     expect(yamsGame([5, 4, 3, 2, 1])).toEqual(40);
