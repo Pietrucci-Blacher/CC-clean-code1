5311742c74bbb4e6c19ee33be8660aefaee13757
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-11-07 14:54:08 +0100

    Feat: Add algorith for YamsGame

--- sources/main.js
+++ sources/main.js
@@ -1,7 +1,24 @@
 const yamsGame = (diceRoll) => {
-  if (!Array.isArray(diceRoll) || diceRoll.length < 5) {
-    throw new Error('diceRoll must be an array with at least 5 elements');
+  const counts = {};
+  let sumDice = 0;
+
+  for (const numberOnDice of diceRoll) {
+    counts[numberOnDice] = (counts[numberOnDice] || 0) + 1;
+    sumDice += numberOnDice;
   }
+
+  const countValues = Object.values(counts);
+  const maxCountOcassion = Math.max(...countValues);
+  const isSuite =
+    new Set(diceRoll).size === 5 &&
+    Math.max(...diceRoll) - Math.min(...diceRoll) === 4;
+
+  if (maxCountOcassion === 5) return 50;
+  if (maxCountOcassion === 4) return 35;
+  if (isSuite) return 40;
+  if (maxCountOcassion === 3) return countValues.includes(2) ? 30 : 28;
+
+  return sumDice;
 };
 
 export default yamsGame;
