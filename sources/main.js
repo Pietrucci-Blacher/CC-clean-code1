04a216f07ec3780f506eceda74e861dc7455777c
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-11-07 15:04:21 +0100

    Feat: Refactor main.js and Yams Algorith with nested functions

--- sources/main.js
+++ sources/main.js
@@ -1,24 +1,40 @@
 const yamsGame = (diceRoll) => {
-  const counts = {};
+  const countsRecursivity = {};
   let sumDice = 0;
 
-  for (const numberOnDice of diceRoll) {
-    counts[numberOnDice] = (counts[numberOnDice] || 0) + 1;
+  const updateCountsAndSum = (numberOnDice) => {
+    countsRecursivity[numberOnDice] =
+      (countsRecursivity[numberOnDice] || 0) + 1;
     sumDice += numberOnDice;
+  };
+
+  const getMaxCountOccasion = () => {
+    const countValues = Object.values(countsRecursivity);
+    return Math.max(...countValues);
+  };
+
+  const isSuite = () => {
+    return (
+      new Set(diceRoll).size === 5 &&
+      Math.max(...diceRoll) - Math.min(...diceRoll) === 4
+    );
+  };
+
+  const checkMaxCountOccasion = () => {
+    const countValues = Object.values(countsRecursivity);
+    const maxCountOcassion = getMaxCountOccasion();
+    if (maxCountOcassion === 5) return 50;
+    if (maxCountOcassion === 4) return 35;
+    if (isSuite()) return 40;
+    if (maxCountOcassion === 3) return countValues.includes(2) ? 30 : 28;
+    return sumDice;
+  };
+
+  for (const numberOnDice of diceRoll) {
+    updateCountsAndSum(numberOnDice);
   }
 
-  const countValues = Object.values(counts);
-  const maxCountOcassion = Math.max(...countValues);
-  const isSuite =
-    new Set(diceRoll).size === 5 &&
-    Math.max(...diceRoll) - Math.min(...diceRoll) === 4;
-
-  if (maxCountOcassion === 5) return 50;
-  if (maxCountOcassion === 4) return 35;
-  if (isSuite) return 40;
-  if (maxCountOcassion === 3) return countValues.includes(2) ? 30 : 28;
-
-  return sumDice;
+  return checkMaxCountOccasion();
 };
 
 export default yamsGame;
