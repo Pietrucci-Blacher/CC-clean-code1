953f1568d52dc55d1270269cf0348f722da2c51c
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-11-07 14:20:32 +0100

    Test: Add test for Full and Grande Suite

--- tests/yams.test.js
+++ tests/yams.test.js
@@ -8,4 +8,10 @@ describe('Yams Game', () => {
   it('the dice suite is a Brelan', () => {
     expect(yamsGame([2, 2, 2, 5, 6])).toEqual(28);
   });
+  it('the dice suite is a Full', () => {
+    expect(yamsGame([2, 2, 3, 3, 3])).toEqual(30);
+  });
+  it('the dice suite is a Grande Suite', () => {
+    expect(yamsGame([5, 4, 3, 2, 1])).toEqual(40);
+  });
 });
